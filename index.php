dba76e2b0071c31a7f97616edffba82151ac822a
LAPTOP-T9VJQ02V\Lenovo <windawan26@gmail.com>
2019-08-07 11:41:52 +0700

    nambah gambar

--- index.php
+++ index.php
@@ -357,7 +357,8 @@
         <div class="row justify-content-center align-items-center">
           <div class="col-md-8 text-center testimony">
             <img src="images/person_5.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
-            <h3 class="mb-4">Yayan Tryana</h3>
+            <h3 class="mb-4">Yayan Tryana
+            </h3>
             <blockquote>
               <p>&ldquo; Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda &rdquo;</p>
             </blockquote>
